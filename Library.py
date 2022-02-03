4bf799b49843fe9e4cba021700931e664c1659c3
soph918 <51512328+soph918@users.noreply.github.com>
2022-02-04 01:08:17 +0900

    Update Library.py

--- Library.py
+++ Library.py
@@ -1,20 +1,20 @@
 lDB = {}
 
-def register():
-    name = input("이름은? : ")
-    number = int(input("핸드폰 번호는? : "))
-    book = "-"
-    lDB[name] = [number, book]
+class Library:
+    def register():
+        name = input("이름은? : ")
+        number = int(input("핸드폰 번호는? : "))
+        book = "-"
+        lDB[name] = [number, book]
 
-def changenumber(name, number):
-    lDB[name] = [number, lDB[name][1]]
+    def changenumber(name, number):
+        lDB[name] = [number, lDB[name][1]]
 
-def delete(name):
-    lDB.pop(name)
+    def delete(name):
+        lDB.pop(name)
 
-def borrowbook(name, book):
-    lDB[name] = [lDB[name][0], book]
+    def borrowbook(name, book):
+        lDB[name] = [lDB[name][0], book]
 
-def returnbook(name):
-    lDB[name] = [lDB[name][0], '-']
-
+    def returnbook(name):
+        lDB[name] = [lDB[name][0], '-']

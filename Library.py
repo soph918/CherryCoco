10b9f8a2c60e32cdd589729b1b546103e8478afc
soph918 <51512328+soph918@users.noreply.github.com>
2022-02-10 23:22:24 +0900

    Update Library.py

--- Library.py
+++ Library.py
@@ -1,22 +1,33 @@
+class Member:
+    name_member = ""
+    phone_number = ""
+    name_book = []
 
-lDB = {}
+    def __init__(self, name, phonenumber, rentbook=[]): # init 왜 필요함? 그냥 아무 메서드 쓰면 안 됨? 차이가 뭐?
+        self.name_member = name
+        self.phone_number = phonenumber
+        self.name_book = rentbook
 
 class Library:
-    def register():
-        name = input("이름은? : ")
-        number = int(input("핸드폰 번호는? : "))
-        book = "-"
-        lDB[name] = [number, book]
+    member_list = []
 
-    def changenumber(name, number):
-        lDB[name] = [number, lDB[name][1]]
-        
-    def delete(name):
-        lDB.pop(name)
+    def regist_member(self, name, phonenumber):
+        new_member = Member(name, phonenumber)
+        self.member_list.append(new_member)
 
-    def borrowbook(name, book):
-        lDB[name] = [lDB[name][0], book]
+    def rent_book(self, name, rentbook):
+        for member in self.member_list:
+            if member.name_member == name:
+                member.name_book.append(rentbook)
 
-    def returnbook(name):
-        lDB[name] = [lDB[name][0], '-']
-        
+    def show_memberlist(self):
+        for member in self.member_list:
+            print(member.name_member, member.phone_number, member.name_book)
+
+library = Library()
+library.regist_member("이주혜", "010-5175-3795")
+library.rent_book("이주혜", "파이썬어려워")
+library.rent_book("이주혜", "살려줘")
+library.regist_member("곱단이", "010-1234-5678")
+library.rent_book("체리코코", "귀여워")
+library.show_memberlist()
